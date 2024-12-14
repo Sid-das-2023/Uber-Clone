f38f44f9308ba0eaa01eb95cc9ef727df7f0fd47
Sidhartha <dassidharthasekhar@gmail.com>
2024-12-14 18:27:44 +0530

    created upto user home

---
+++ Frontend/src/components/VehiclePanel.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const VehiclePanel = (props) => {
+  return (
+    <div>
+      <h5 onClick={() => {props.setVehiclePanel(false)}} className='p-2 text-center absolute top-0 w-[93%]'><i className="text-xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
+        <h3 className="text-3xl font-bold mb-5">Choose a vehicle</h3>
+      <div className="w-full p-1 mb-2 flex items-center justify-between border-2 active:border-black rounded-xl">
+          <img className='h-14' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ7Kt54z31PkbdlqmqnyWnaCjvcLYRG-T_8Q&s" alt="Car" />
+          <div className="w-1/2">
+            <h4 className="font-medium text-lg">Car <span className="text-sm"><i className="ri-user-3-fill text-sm"></i>4</span></h4>
+            <h5 className="font-medium text-sm">2 mins away</h5>
+            <p className="font-base text-xs text-gray-500">Affordable compact rides</p>
+          </div>
+          <h2 className="text-lg font-semibold">$22.50</h2>
+        </div>
+        <div className="w-full p-3 mb-2 flex items-center justify-between border-2 active:border-black rounded-xl">
+          <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="Bike" />
+          <div className="ml-8 w-1/2">
+            <h4 className="font-medium text-lg">Bike <span className="text-sm"><i className="ri-user-3-fill text-sm"></i>1</span></h4>
+            <h5 className="font-medium text-sm">2 mins away</h5>
+            <p className="font-base text-xs text-gray-500">Affordable bike rides</p>
+          </div>
+          <h2 className="text-lg font-semibold">$10.50</h2>
+        </div>
+        <div className="w-full p-3 mb-2 flex items-center justify-between border-2 active:border-black rounded-xl">
+          <img className='h-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="Auto" />
+          <div className="ml-8 w-1/2">
+            <h4 className="font-medium text-lg">Auto <span className="text-sm"><i className="ri-user-3-fill text-sm"></i>3</span></h4>
+            <h5 className="font-medium text-sm">2 mins away</h5>
+            <p className="font-base text-xs text-gray-500">Affordable auto rides</p>
+          </div>
+          <h2 className="text-lg font-semibold">$15.00</h2>
+        </div>
+    </div>
+  )
+}
+
+export default VehiclePanel
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,110 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import "remixicon/fonts/remixicon.css";
+import LocationSearchPanel from "../components/LocationSearchPanel";
+import VehiclePanel from "../components/VehiclePanel";
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+  const [pickup, setPickup] = useState("");
+  const [destination, setDestination] = useState("");
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);
+  const panelCloseRef = useRef(null);
+  const [vehiclePanel, setVehiclePanel] = useState(false);
+  const vechiclePanelRef = useRef(null);
 
-export default Home
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: "70%",
+      });
+      gsap.to(panelCloseRef.current, {
+        opacity: 1,
+      });
+    } else {
+      gsap.to(panelRef.current, {
+        height: 0,
+      });
+      gsap.to(panelCloseRef.current, {
+        opacity: 0,
+      });
+    }
+  }, [panelOpen]);
+
+  useEffect(() => {
+    if(vehiclePanel) {
+      gsap.to(vechiclePanelRef.current, {
+        transform: "translateY(0%)",
+      });
+    } else {
+      gsap.to(vechiclePanelRef.current, {
+        transform: "translateY(100%)",
+      });
+    }
+  }, [vehiclePanel])
+
+  return (
+    <div className="h-screen relative overflow-hidden">
+      <img
+        className="w-14 absolute left-5 top-5"
+        src="https://freelogopng.com/images/all_img/1659761100uber-logo-png.png"
+        alt="Uber Logo"
+      />
+      <div className="h-screen w-screen">
+        {/* image for temporary use */}
+        <img
+          className="h-full w-full object-cover"
+          src="https://media.geeksforgeeks.org/wp-content/uploads/20220218205322/WhatsAppImage20220218at54912PM-304x660.jpeg"
+          alt="Background"
+        />
+      </div>
+      <div className="h-screen flex flex-col justify-end absolute top-0 w-full">
+        <div className="h-[30%] bg-white p-5 relative rounded-lg">
+          <h5
+            ref={panelCloseRef}
+            onClick={() => {
+              setPanelOpen(false);
+            }}
+            className="absolute opacity-0 right-6 top-4 text-2xl"
+          >
+            <i className="ri-arrow-down-wide-line"></i>
+          </h5>
+          <h4 className="text-2xl font-bold">Find a trip</h4>
+          <form onSubmit={submitHandler}>
+            <div className="line absolute h-16 w-1 top-[40%] left-10 bg-gray-900 rounded-full"></div>
+            <input
+              onClick={() => setPanelOpen(true)}
+              value={pickup}
+              onChange={(e) => setPickup(e.target.value)}
+              type="text"
+              placeholder="Add a pick-up location"
+              className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-3"
+            />
+            <input
+              onClick={() => setPanelOpen(true)}
+              value={destination}
+              onChange={(e) => setDestination(e.target.value)}
+              type="text"
+              placeholder="Enter your destination"
+              className="bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-3"
+            />
+          </form>
+        </div>
+        {/* we can give height 0 when not needed and then h-[70%] once clicked */}
+        <div ref={panelRef} className="h-[70%] bg-white">
+          <LocationSearchPanel setPanelOpen={setPanelOpen} setVehiclePanel={setVehiclePanel}/>
+        </div>
+      </div>
+      <div ref={vechiclePanelRef} className="fixed w-full px-3 py-10 pt-12 z-10 bottom-0 bg-white translate-y-full">
+        <VehiclePanel setVehiclePanel={setVehiclePanel}/>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Home;
